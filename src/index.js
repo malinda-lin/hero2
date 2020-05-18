9fee0f9351ad7ac81f44e38194830320e567ed06
Xiaoqing-Lin <57362207+Xiaoqing-Lin@users.noreply.github.com>
2020-05-17 21:10:23 -0400

    none

---
+++ src/index.js
@@ -0,0 +1,6 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import REACTDOM from 'react-dom';
+
+console.log('hi! hellow?');
+REACTDOM.render(<h1>hello!</h1>, document.getElementById('app'));
